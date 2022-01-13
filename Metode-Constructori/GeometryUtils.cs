facfc6e07d6414f79345f629e882038f532c871a
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2022-01-13 22:31:10 +0200

    Gata

---
+++ Metode-Constructori/GeometryUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metode_Constructori
+{
+    public static class GeometryUtils
+    {
+        public static void Point(Point[] points, int dx, int dy)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].Move(dx, dy); 
+            }
+        }
+        public static void Move(Triangle triunghi, int dx, int dy)
+        {
+            triunghi.A.X = triunghi.A.X + dx;
+            triunghi.B.X = triunghi.B.X + dx;
+            triunghi.C.X = triunghi.C.X + dx;
+            triunghi.A.Y = triunghi.A.Y + dy;
+            triunghi.B.Y = triunghi.B.Y + dy;
+            triunghi.C.Y = triunghi.C.Y + dy;
+        }
+        public static void Move(Rectangle dreptunghi, int dx, int dy)
+        {
+            dreptunghi.A.X = dreptunghi.A.X + dx;
+            dreptunghi.B.X = dreptunghi.B.X + dx;
+            dreptunghi.C.X = dreptunghi.C.X + dx;
+            dreptunghi.D.X = dreptunghi.D.X + dx;
+            dreptunghi.A.Y = dreptunghi.A.Y + dy;
+            dreptunghi.B.Y = dreptunghi.B.Y + dy;
+            dreptunghi.C.Y = dreptunghi.C.Y + dy;
+            dreptunghi.D.Y = dreptunghi.D.Y + dy;
+        }
+        public static void Print(Point[] points)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine($"X[{i}]=" + points[i].X);
+                Console.WriteLine($"Y[{i}]=" + points[i].Y);
+            }
+        }
+        public static void Print(Triangle triunghi)
+        {
+            Console.WriteLine("A " + triunghi.A.X + " " + triunghi.A.Y);
+            Console.WriteLine("B " + triunghi.B.X + " " + triunghi.B.Y);
+            Console.WriteLine("C " + triunghi.C.X + " " + triunghi.C.Y);
+        }
+        public static void Print(Rectangle dreptunghi)
+        {
+            Console.WriteLine("A " + dreptunghi.A.X + " " + dreptunghi.A.Y);
+            Console.WriteLine("B " + dreptunghi.B.X + " " + dreptunghi.B.Y);
+            Console.WriteLine("C " + dreptunghi.C.X + " " + dreptunghi.C.Y);
+            Console.WriteLine("D " + dreptunghi.D.X + " " + dreptunghi.D.Y);
+        }
+    }
+}
--- Metode-Constructori/Program.cs
+++ Metode-Constructori/Program.cs
@@ -6,7 +6,19 @@ namespace Metode_Constructori
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Point p1 = new Point (1, 5);
+            Point p2 = new Point(5, 5);
+            Point p3 = new Point(5, 2);
+            Point p4 = new Point(1, 2);
+            Triangle triunghi = new Triangle ( p1, p2, p3 );
+            triunghi.Print();
+            triunghi.Move(0, -1);
+            triunghi.Print();
+            //Rectangle dreptunghi = new Rectangle(p1, p3);
+            //dreptunghi.Move(1, 1);
+            //dreptunghi.Print();
+            GeometryUtils.Print(triunghi);
+            GeometryUtils.Print(new Point[] {p1, p2, p3 });
         }
     }
 }
